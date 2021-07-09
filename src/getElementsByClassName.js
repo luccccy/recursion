9054b7c86f64bb4e5949936dae2b6793f90aba66
lucyjunejack <gkvoid3@gmail.com>
2021-07-09 10:01:44 -0700

    Assignment Completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,19 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var nodeList = [];
+  var iterateBody = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      nodeList.push(node);
+    }
+    if (node.hasChildNodes()) {
+      var children = node.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        iterateBody(children[i]);
+      }
+    }
+  };
+  iterateBody(document.body);
+  return nodeList;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arrEle = [];
+  var objEle = [];
+  var objKey = [];
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    if (obj[0] === undefined) {
+      return '[]';
+    } else {
+      for (var i = 0, len = obj.length; i < len; i++) {
+        arrEle.push(stringifyJSON(obj[i]));
+      }
+      return '[' + arrEle + ']';
+    }
+  } else if (obj instanceof Object) {
+    objKey = Object.keys(obj);
+    objKey.forEach(function(key) {
+      var keyOutput = '"' + key + '":';
+      var keyValueOutput = obj[key];
+      if (keyValueOutput instanceof Function || typeof keyValueOutput === undefined) {
+        objEle.push('');
+      } else if (typeof keyValueOutput === 'string') {
+        objEle.push(keyOutput + '"' + keyValueOutput + '"');
+      } else if (typeof keyValueOutput === 'boolean'
+      || typeof keyValueOutput === 'number'
+      || keyValueOutput === null) {
+        objEle.push(keyOutput + keyValueOutput);
+      } else if (keyValueOutput instanceof Object) {
+        objEle.push(keyOutput + stringifyJSON(keyValueOutput));
+      }
+    });
+    return '{' + objEle + '}';
+  }
 };
